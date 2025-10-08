315e159c12728da8ea16466640823203602d9458
WawiRhiksaWibawa <wawirhiksa985@gmail.com>
2025-10-09 00:27:28 +0700

    Update Our Service

--- resources/views/components/kontak.blade.php
+++ resources/views/components/kontak.blade.php
@@ -32,7 +32,8 @@
                         <div class="bg-slate-100 p-3 rounded-full group-hover:bg-slate-200 transition-all duration-300">
                             <svg class="w-6 h-6 text-slate-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
                         </div>
-                        <span class="text-gray-700 font-medium">BRI Tower, Bandung</span>
+                        <span class="text-gray-700 font-medium">Menara BRI Bandung
+Jl. Asia Afrika No.57, Kota Bandung</span>
                     </div>
 
                     {{-- Telepon --}}
