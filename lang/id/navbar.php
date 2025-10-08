7067ce36c24c5f96f0c4009e73ec6ee9ac229503
sahlhafidzirkhami <sahlhafidzirkhami@gmail.com>
2025-10-08 15:11:43 +0700

    feat(layout):add login and register

---
+++ lang/id/navbar.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'services' => 'Layanan',
+    'procedure' => 'Prosedur',
+    'about' => 'Tentang',
+    'why_us' => 'Mengapa Kami',
+    'contact' => 'Kontak',
+    'contact_us_title' => 'Hubungi Kami',
+    'contact_us_subtitle' => 'Kami siap memproses informasi untuk berdiskusi lebih lanjut mengenai proyek Anda.',
+];
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,30 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\MessageController;
 Route::get('/', function () {
     return view('home',['title' => 'Home']);
 });
 
+Route::get('/language/{locale}', function ($locale) {
+    // Simpan pilihan bahasa ke dalam session
+    session()->put('locale', $locale);
+    // Arahkan pengguna kembali ke halaman sebelumnya
+    return redirect()->back();
+})->name('language.switch');
+
 Route::get('/messages', function () {
     return view('messages');
 });
-Route::resource('messages', MessageController::class);
+Route::resource('messages', MessageController::class)->middleware('auth');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
